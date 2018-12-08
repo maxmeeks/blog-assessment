788f4d4c45bad164bd032fe8f5b603fd1c1438d3
Max <meekhoffm@gmail.com>
2018-12-08 13:01:59 -0500

    initializing

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-var argv = require('yargs').argv;
-var git = require('gulp-git');
-var runSequence = require('run-sequence');
 
 gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.scss')
@@ -47,41 +44,6 @@ gulp.task('vendor', function() {
         '!./node_modules/jquery/dist/core.js'
       ])
       .pipe(gulp.dest('./vendor/jquery'))
-  
-    gulp.src([
-        './node_modules/jquery.easing/*.js'
-      ])
-      .pipe(gulp.dest('./vendor/jquery-easing'))
-  
-  });
-
-  gulp.task('init', function() {
-    console.log(argv.m);
-  });
-  
-  gulp.task('add', function() {
-    console.log('adding...');
-    return gulp.src('.')
-      .pipe(git.add());
-  });
-  
-  gulp.task('commit', function() {
-    console.log('commiting');
-    if (argv.m) {
-      return gulp.src('.')
-        .pipe(git.commit(argv.m));
-    }
-  });
-  
-  gulp.task('push', function(){
-    console.log('pushing...');
-    git.push('origin', 'master', function (err) {
-      if (err) throw err;
-    });
-  });
-  
-  gulp.task('gitsend', function() {
-    runSequence('add', 'commit', 'push');
-  });
+});
 
 
